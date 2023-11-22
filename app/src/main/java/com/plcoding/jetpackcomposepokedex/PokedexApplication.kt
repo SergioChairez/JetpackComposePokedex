0632c258cd26aea4eddc9f7501b8f3d02510b4d9
sergio.chairez <sergio.chairez@accenture.com>
2023-11-22 17:03:46 -0600

    Second Commit

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/PokedexApplication.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/PokedexApplication.kt
@@ -1,8 +1,10 @@
 package com.plcoding.jetpackcomposepokedex
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class PokedexApplication : Application() {
 
     override fun onCreate() {
---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/models/PokedexListEntry.kt
@@ -0,0 +1,7 @@
+package com.plcoding.jetpackcomposepokedex.data.models
+
+class PokedexListEntry (
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int,
+)
